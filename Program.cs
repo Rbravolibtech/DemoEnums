5cb4cbca16e8808d910de31a5578c8777a33f408
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-11 12:34:26 -0700

    using Enums and learning how to call something specific  when you console log it

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DemoEnums
+{
+    public enum ShippingMethod //what ever you name here make sure to name it down on the bottom of code
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail=  2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express; //So you use shippingMethod here
+
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId); //also here use shipping method
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
